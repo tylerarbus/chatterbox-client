d95114c494a33e5dc727d5b3f631682655d6a5f7
Andy Yeo <andy.yeo@gmail.com>
2017-02-17 15:54:53 -0800

    Passed test for adding messages to DOM on app.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,6 @@
 var app = {
   init() {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
+
   },
   send(message) {
     $.ajax({
@@ -35,7 +22,7 @@ var app = {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: undefined,
-      type: 'POST',
+      type: 'GET',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
@@ -49,7 +36,11 @@ var app = {
   },
   clearMessages() {
     $('#chats').empty();
-  }
+  },
+  renderMessage(message) {
+    var $chat = '<div id="message"></div>';
+    $('#chats').append($chat);
+  }  
 };
 
 
