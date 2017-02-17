017745632a9236c323e91863593b6b9df8c840d2
Andy Yeo <andy.yeo@gmail.com>
2017-02-17 12:48:21 -0800

    Passed test for submit a GET request in app.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -32,7 +32,20 @@ var app = {
     });
   },
   fetch() {
-    
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: undefined,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
   }
 };
 
